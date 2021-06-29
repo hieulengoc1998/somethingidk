4d4f9cef5c36de47faebbc7c25a824738cc83916
hieulengoc1998 <86396599+hieulengoc1998@users.noreply.github.com>
2021-06-29 08:14:49 -0700

    third comment

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 let stuff = ['adidas', 'gucci', 'converse', 'chanel']
-let option = prompt('What do u want to do? (C,R,U,D')
-if (option === "C") {
+let input = prompt('What do u want to do? (C,R,U,D)')
+option = input
+if (option === "C" || option === "c") {
     let new_item = prompt("Name your new stuff")
     stuff.push(new_item)
-}else if (option === "R") {
-    console.log("This is your stuff");
-}else if (option === "U" ) {
+}else if(option === "R" || option === "r"){
+    alert("This is your stuff");
+}else if(option === "U" || option === "u"){
     index = prompt("Enter the position you want to update")
-    new_value = prompt('enter new name')
-    stuff[index] = new_value
-}else if(option === "D"){
+    if(index > 3) {
+        alert("Please enter a pos lower than 3")
+    }else {
+        new_value = prompt('enter new name')
+        stuff[index] = new_value
+    }
+}else if(option === "D" || option === "d" ){
     let index = prompt("Enter the position you want to delete")
-    stuff.splice(index, 1)
+    if(index > 3){
+        alert("Please enter a pos lower than 3")
+    }else{
+        let deleteCount = prompt("How many do u want to delete?")
+        stuff.splice(index, deleteCount)
+    }
+    
+}else {
+    alert("Try again!")
 }
 
 for(let i = 0; i < stuff.length; i++){
