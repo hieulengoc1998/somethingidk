9203900b67b47c6b61ed59cdf378469b94f9416b
hieulengoc1998 <86396599+hieulengoc1998@users.noreply.github.com>
2021-06-30 22:13:08 -0700

    bt 30/6

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-let stuff = ['adidas', 'gucci', 'converse', 'chanel']
-let input = prompt('What do u want to do? (C,R,U,D)')
-option = input.toUpperCase();
-console.log(option)
-if (option === "C") {
-    let new_item = prompt("Name your new stuff")
-    stuff.push(new_item)
-}else if(option === "R"){
-    alert("This is your stuff");
-}else if(option === "U"){
-    index = prompt("Enter the position you want to update")
-    if(index > 3) {
-        alert("Please enter a pos lower than 3")
-    }else {
-        new_value = prompt('enter new name')
-        stuff[index] = new_value
-    }
-}else if(option === "D"){
-    let index = prompt("Enter the position you want to delete")
-    if(index > 3){
-        alert("Please enter a pos lower than 3")
-    }else{
-        let deleteCount = prompt("How many do u want to delete?")
-        stuff.splice(index, deleteCount)
-    }    
-}else {
-    alert("Try again!")
-}
-for(let i = 0; i < stuff.length; i++){
-    console.log(i, stuff[i]);
-}
+let input = document.getElementById("textInput")
+let output = document.getElementById("textOutput")
+
+input.addEventListener("input", () => {
+    console.log(input.value)
+    let outputValue = document.createElement('p')
+    outputValue.innerHTML = input.value;
+    output.appendChild(outputValue)
+})
